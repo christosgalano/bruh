ea6ab8a1bc78841ce3d6cd95ec6a1257541336cd
Christos Galanopoulos <xrholy11@gmail.com>
2023-06-28 00:04:25 +0300

    Add includePreview option

--- internal/apiversions/apiversions.go
+++ internal/apiversions/apiversions.go
@@ -12,8 +12,14 @@ import (
 	"sort"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/christosgalano/bruh/internal/types"
+)
+
+const (
+	// dateFormat is the format used for parsing the dates in the API versions.
+	dateFormat = "2006-01-02"
 )
 
 // fetchResourcePage fetches the HTML content of a given URL.
@@ -32,7 +38,7 @@ func fetchResourcePage(url string) (string, error) {
 	return string(body), nil
 }
 
-// extractAPIVersions extracts and returns all the API versions sorted using a regex pattern.
+// extractAPIVersions extracts all the API versions using a regex pattern and returns them sorted in descending order.
 func extractAPIVersions(body string, pattern string) ([]string, error) {
 	versions := []string{}
 
@@ -47,18 +53,45 @@ func extractAPIVersions(body string, pattern string) ([]string, error) {
 		return nil, fmt.Errorf("no API versions found")
 	}
 
-	// Sort the versions in descending order (newest first)
+	// Sort the versions in descending order (newest first).
+	// Non-preview versions should be sorted after preview versions.
+	// For example, "2021-02-02-preview" should be after "2021-02-02".
 	sort.Slice(versions, func(i, j int) bool {
-		return versions[i] > versions[j]
+		// Extract the dates from the strings
+		dateI := strings.Split(versions[i], "-preview")[0]
+		dateJ := strings.Split(versions[j], "-preview")[0]
+
+		// Parse the dates
+		parsedDateI, errorI := time.Parse(dateFormat, dateI)
+		parsedDateJ, errorJ := time.Parse(dateFormat, dateJ)
+
+		// If the dates cannot be parsed, return the string comparison
+		if errorI != nil || errorJ != nil {
+			return versions[i] > versions[j]
+		}
+
+		// Compare the dates
+		if parsedDateI.Equal(parsedDateJ) {
+			return !strings.HasSuffix(versions[i], "-preview")
+		}
+
+		return parsedDateI.After(parsedDateJ)
 	})
 
 	return versions, nil
 }
 
 // UpdateResource updates the available API versions for a given resource.
-func UpdateResource(resource *types.Resource) error {
+// If includePreview is true, preview API versions will be included.
+func UpdateResource(resource *types.Resource, includePreview bool) error {
 	url := "https://learn.microsoft.com/en-us/azure/templates/" + strings.ToLower(resource.Namespace) + "/" + strings.ToLower(resource.Name)
-	pattern := `href="(\d{4}-\d{2}-\d{2}-preview|\d{4}-\d{2}-\d{2})/` + strings.ToLower(resource.Name) + `"`
+
+	var pattern string
+	if includePreview {
+		pattern = `href="(\d{4}-\d{2}-\d{2}-preview|\d{4}-\d{2}-\d{2})/` + strings.ToLower(resource.Name) + `"`
+	} else {
+		pattern = `href="(\d{4}-\d{2}-\d{2})/` + strings.ToLower(resource.Name) + `"`
+	}
 
 	body, err := fetchResourcePage(url)
 	if err != nil {
@@ -76,9 +109,10 @@ func UpdateResource(resource *types.Resource) error {
 }
 
 // UpdateBicepFile updates the available API versions for all resources in a given bicep file.
-func UpdateBicepFile(bicepFile *types.BicepFile) error {
+// If includePreview is true, preview API versions will be included.
+func UpdateBicepFile(bicepFile *types.BicepFile, includePreview bool) error {
 	for i := range bicepFile.Resources {
-		err := UpdateResource(&bicepFile.Resources[i])
+		err := UpdateResource(&bicepFile.Resources[i], includePreview)
 		if err != nil {
 			return err
 		}
@@ -87,7 +121,8 @@ func UpdateBicepFile(bicepFile *types.BicepFile) error {
 }
 
 // UpdateBicepDirectory updates the available API versions for all resources in all bicep files of a given bicep directory.
-func UpdateBicepDirectory(bicepDirectory *types.BicepDirectory) error {
+// If includePreview is true, preview API versions will be included.
+func UpdateBicepDirectory(bicepDirectory *types.BicepDirectory, includePreview bool) error {
 	// Create a wait group to wait for all goroutines to finish
 	var wg sync.WaitGroup
 
@@ -98,7 +133,7 @@ func UpdateBicepDirectory(bicepDirectory *types.BicepDirectory) error {
 		wg.Add(1)
 		go func(file *types.BicepFile) {
 			defer wg.Done()
-			err := UpdateBicepFile(file)
+			err := UpdateBicepFile(file, includePreview)
 			if err != nil {
 				results <- err
 			}
